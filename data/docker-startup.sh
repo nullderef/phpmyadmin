71de45db03c4a2c47e7afe532d467de053fd0cb1
Michael Bramsiepe <siepieuk@hotmail.co.uk>
2017-11-11 18:15:02 +0000

    Changed default permissions on config file

--- data/docker-startup.sh
+++ data/docker-startup.sh
@@ -18,7 +18,7 @@ sed -i "s:BLOWFISH_SECRET:$BLOWFISH_SECRET:g" "/phpmyadmin/config/config.inc.php
 sed -i "s:HOSTNAME:$HOSTNAME:g" "/phpmyadmin/config/config.inc.php"
 
 chown -h www-data:www-data /phpmyadmin/config/config.inc.php
-chmod 777 /phpmyadmin/config/config.inc.php
+chmod 644 /phpmyadmin/config/config.inc.php
 
 /etc/init.d/php7.0-fpm start
 /etc/init.d/nginx start
